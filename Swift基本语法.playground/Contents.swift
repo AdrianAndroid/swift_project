f30ece54feb6ff71b6a7e61105e9de113b0a6bb2
zhaojian2 <zhaojian2@joyy.com>
2022-03-18 11:43:10 +0800

    添加基础的一些东西

---
+++ Swift基本语法.playground/Contents.swift
@@ -0,0 +1,60 @@
+// 资料
+// https://www.runoob.com/swift/swift-basic-syntax.html
+
+import UIKit
+
+var greeting = "Hello, playground"
+print(greeting)
+
+print
+(
+    "test!"
+)
+
+// 这是一行注释
+/* 这也是条注释，
+ 但跨越多行*/
+/*这是第一个多行注释的开头
+ /*这是嵌套的第二个多行注释*/
+这是第一个多行注释的结尾 */
+
+
+// 分号
+/*我的第一个Swift程序*/
+var myString = "Hello, World!"; print(myString)
+
+// 关键字
+// 与声明有关的关键字
+// class      deinit    enum      extension
+// func       import    init      internal
+// let        operator  private   protocol
+// public     static    struct    subscript
+// typealias  var
+//
+// 与语句有关的关键字
+// break  case   continue     default
+// do     else   fallthrough  for
+// if     in     return       switch
+// where  while
+//
+// 表达式和类型关键字
+// as      dynamicType   false   is
+// nil     self          Self    super
+// true    _COLUMN_      _FILE_  _FUNCTION_
+// _LINE_
+//
+// 在特定上下文中使用的关键字
+// associativity convenience   dynamic     didSet
+// final         get           infix       inout
+// lazy          left          mutating    none
+// nonmutating   optional      override    postfix
+// precedence    prefix        Protocol    required
+// right         set           Type        unowned
+// weak          willSet
+
+
+// Swift字面量
+//42              // 整型字面量
+//3.14159         // 浮点型字面量
+//"Hello, world!" // 字符串型字面量
+//true            // 布尔型字面量
